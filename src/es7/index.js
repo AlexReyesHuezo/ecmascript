c9429306eb927b5e59f6494b140ff25bfc61a6bb
Alex Reyes <73624939+AARH777@users.noreply.github.com>
2022-03-15 14:35:04 -0600

    Features in ES7: Includes function & Power operator

---
+++ src/es7/index.js
@@ -0,0 +1,19 @@
+// Includes (Incluye)
+// ------------------------------------------------------------
+
+let numbers = [1, 2, 3, 4, 5];
+
+if (numbers.includes(1)) {
+    console.log('The array includes the number 1');
+}
+else {
+    console.log('The array does not include the number 1');
+}
+
+
+// Power operator (Operador de potencia)
+// ------------------------------------------------------------
+
+let base = 2;
+let exponent = 3;
+let result = base ** exponent;
