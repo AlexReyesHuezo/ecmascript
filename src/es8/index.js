dffcb97a6aeba5a9d104d07a12fb98bb86dba2aa
Alex Reyes <73624939+AARH777@users.noreply.github.com>
2022-03-16 19:04:44 -0600

    Features in ES8: Async Await
    Using Async Await within Promises an Try Catch

--- src/es8/index.js
+++ src/es8/index.js
@@ -54,3 +54,33 @@ const fantastic = {
 const fabled = {
     '🦄': 'unicorn',
 }
+
+
+// Async await (Espera asíncrona)
+// ------------------------------------------------------------
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve('Hello, world!'), 3000)
+            : reject(new Error('Test error'));
+    });
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+};
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+anotherFunction();
