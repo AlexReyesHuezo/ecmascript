c69b07eae83fc0d8047a13472982fba3d1affd94
Alex Reyes <73624939+AARH777@users.noreply.github.com>
2022-06-08 16:53:10 -0600

    Features in ES10: Adding Array.flatMap() and trimStart() with trimEnd() methods and 'catch' without error parameter

--- src/es10/index.js
+++ src/es10/index.js
@@ -7,3 +7,41 @@ let array = [1, 2, 3, [1, 2, 3, [1, 2, 3]]];
 console.log(array.flat()); // the depth of flattening is 1 by default
 
 console.table(array.flat(2)); // the depth of flattening is 2
+
+// Array Flat Map (Matriz plana y mapeada)
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/flatMap
+// ---------------------------------------------------------------------------------------------
+
+let numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.flatMap(number => [number, number * 2]));
+
+// Trim Start y Trim End (Delete spaces at the start and end of a string)
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/String/trimStart
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/String/trimEnd
+// ---------------------------------------------------------------------------------------------
+
+let hello = "   Hello World!  ";
+
+console.log(hello);
+
+console.log(hello.trimStart().trimEnd());
+
+// Try Catch without Error parameter
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Try_catch
+// ---------------------------------------------------------------------------------------------
+
+// Before
+
+try {
+    console.log("Hello World!");
+} catch (error) {
+    console.log(error);
+}
+
+// after
+try {
+    console.log("Hello World!");
+} catch {
+    console.log(error);
+}
