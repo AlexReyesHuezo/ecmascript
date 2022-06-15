7d9f01c915b26b27dfcce6ee8c3306ed181b96ca
Alex Reyes <73624939+AARH777@users.noreply.github.com>
2022-06-15 16:06:26 -0600

    Features in ES11: BigInt, Promise.allSettled and global.This

--- src/es11/index.js
+++ src/es11/index.js
@@ -8,3 +8,38 @@ button.addEventListener('click', async function () {
     const module = await import('./module.js');
     module.hello();
 });
+
+
+// BigInt
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
+// --------------------------------------------------------------------------------------------
+
+const bigInt = BigInt(1234567890123456789012345678901234567890);
+const anotherBigInt = 1234567890123456789012345678901234567890n;
+
+console.log(bigInt);
+console.log(anotherBigInt);
+
+
+// Promise.allSettled()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/allSettled
+// --------------------------------------------------------------------------------------------
+
+// unlike promise.all, this method returns a single promise that resolves when all the promises in the array have resolved or reject.
+
+const promise1 = new Promise((resolve, reject) => reject('error'));
+const promise2 = new Promise((resolve, reject) => resolve('success'));
+const promise3 = new Promise((resolve, reject) => reject('error'));
+const promise4 = new Promise((resolve, reject) => resolve('success'));
+const promise5 = new Promise((resolve, reject) => resolve('success'));
+
+Promise.allSettled([promise1, promise2, promise3, promise4, promise5])
+    .then(results => console.log(results));
+
+
+// globalThis
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis
+// --------------------------------------------------------------------------------------------
+
+console.log(window);
+console.log(globalThis); // It's the same as window in the browser.
